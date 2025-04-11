6ad6983a6911112f5aa845772d06405f0622cc26
kescobo <kevbonham@gmail.com>
2025-04-11 14:43:28 +0000

    Deploying to gh-pages from @ BonhamLab/bonhamlab.github.io@0456b067b45476bf4610e44089d1d22fe58492da 🚀

--- assets/index/code/_ceval_1.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-x = y = z = q = true; #hide
-x = y && (z || q)
-println("julia")
--- config.jl
+++
@@ -1,2 +0,0 @@
-site_title = "Bonham Lab"
-site_desc = "Website for the Bonham Lab at Tufts Medical Center"
