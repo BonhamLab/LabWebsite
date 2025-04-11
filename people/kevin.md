b1ecafe2e3e79eef55c4d8eec4e67e0ce60fe06d
Kevin Bonham <dev@bonham.ch>
2025-04-11 10:38:49 -0400

    Better profile pages

--- people/kevin.md
+++ people/kevin.md
@@ -3,6 +3,7 @@ using Dates
 name="Kevin Bonham"
 title="Assistant Professor"
 vitae="Benevolant dictator"
+portrait="/assets/kevin_headshot.png"
 email="kevin.bonham@tuftsmedicine.org"
 joined = Date(2025,02,01)
 +++
--- utils.jl
+++ utils.jl
@@ -19,14 +19,14 @@ function hfun_list_people()
         (
         node("div", class="card-column",
             node("div", class="card-body", 
-                node("img", src="/assets/portrait_placeholder.png"),
+                node("img", src=p.portrait),
                 node("div", class="card-container", 
-                     node("h2", node("a", href=p.href, p.name)),
+                    node("h2", node("a", href=p.href, p.name)),
                     node("div", class="card-title", p.title),
                     node("div", class="card-vitae", p.vitae),
-                    node("div", class="card-email", p.email),
-                    node("p", node("a", href="mailto:$(p.email)",
-                            node("button", class="card-button", "Contact")
+                    node("div", class="card-email", node("a", href="mailto:$(p.email)", p.email)),
+                    node("p", node("a", href=p.href,
+                            node("button", class="card-button", "Details")
                            )
                     )
                 )
@@ -34,6 +34,20 @@ function hfun_list_people()
            ) for p in get_people() if !p.alumn
         )...
         )
+    )
+end
+
+function person_info(rp)
+    return (;
+    date=getvarfrom(:joined, rp),
+    name=getvarfrom(:name, rp),
+    title=getvarfrom(:title, rp),
+    email=getvarfrom(:email, rp),
+    portrait=getvarfrom(:portrait, rp, "/assets/portrait_placeholder.png"),
+    vitae=getvarfrom(:vitae, rp),
+    alumn=getvarfrom(:alumn, rp, false),
+    href="/$(splitext(rp)[1])",
+    tags=get_page_tags(rp)
     )
 end
 
@@ -46,27 +60,31 @@ function get_people(basepath::String="people")
         append!(paths, joinpath.(root, files))
     end
     # for each of those people, get their info
-    posts = [
-        (
-        date=getvarfrom(:joined, rp),
-        name=getvarfrom(:name, rp),
-        title=getvarfrom(:title, rp),
-        email=getvarfrom(:email, rp),
-        vitae=getvarfrom(:vitae, rp),
-        alumn=getvarfrom(:alumn, rp, false),
-        href="/$(splitext(rp)[1])",
-        tags=get_page_tags(rp)
-        )
-        for rp in paths
-    ]
+    posts = [person_info(rp) for rp in paths]
     sort!(posts, by=x -> x.date)
    
     return posts
 end
 
 function hfun_person_header()
-    name = getlvar(:name, getlvar(:title))
-    return string(node("div", class="franklin-content", node("h1", name)))
+    person = person_info(get_rpath())
+    return string(node("div", class="franklin-content", 
+
+        node("div", class="profile-header",
+            node("div", class="profile-info",
+                node("h1", class="profile-name", person.name),
+                node("div", class="profile-title", person.title),
+                node("div", class="profile-vitae", person.vitae),                    
+                node("div", class="profile-email",
+                     node("a", href="mailto:$(person.email)", person.email)
+                ),
+            ),
+            node("div", class="profile-image-container",
+                 node("img", class="profile-image", src=person.portrait, alt="$(person.name)")
+            )
+        ),
+
+    ))
 end
 
 function hfun_list_projects()
