29c3be5ebda375dad7d5c4dd408ec6779d825021
Kevin Bonham <dev@bonham.ch>
2026-04-16 09:43:33 -0400

    update lab logo

--- config.jl
+++ config.jl
@@ -1,6 +1,6 @@
 website_title = "Bonham Lab"
 website_descr = "Website for the Bonham Lab at Tufts Medical Center"
-website_url = "http://bonhamlab.github.io/"
+website_url = "http://bonhamlab.bio/"
 
 author = "Kevin Bonham, PhD"
 
